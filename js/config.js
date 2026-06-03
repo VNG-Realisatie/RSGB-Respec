3dc460ddca812993a3a198c6821c56b0580c775d
Robert Melskens <robert.melskens@vng.nl>
2026-06-03 15:38:51 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -47,32 +47,32 @@ let respecConfig = {
   edDraftURI: "https://vng-realisatie.github.io/RSGB-Respec/",
   latestVersion: "https://vng-realisatie.github.io/RSGB-Respec/",
 
-  issueBase: "https://github.com/VNG-Realisatie/Actualisering-RSGB",
+//  issueBase: "https://github.com/VNG-Realisatie/Actualisering-RSGB",
 
-  //otherLinks: [{
-  //  key: "Doe mee",
-  //  data: [{
-  //    value: "VNG-Realisatie/Actualisering-RSGB",
-  //    href: "https://github.com/VNG-Realisatie/Actualisering-RSGB"
-  //  },
-  //  {
-  //    value: "All issues",
-  //    href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/issues"
-  //  },
-  //  {
-  //    value: "Dien een melding in",
-  //    href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/issues/new/choose"
-  //  },
-  //  {
-  //    value: "Revisiehistorie",
-  //    href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/commits"
-  //  },
-  //  {
-  //    value: "Pull requests",
-  //    href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/pulls"
-  //  },
-  //  ]
-  //}],
+  otherLinks: [{
+    key: "Doe mee",
+    data: [{
+      value: "VNG-Realisatie/Actualisering-RSGB",
+      href: "https://github.com/VNG-Realisatie/Actualisering-RSGB"
+    },
+    {
+      value: "All issues",
+      href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/issues"
+    },
+    {
+      value: "Dien een melding in",
+      href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/issues/new/choose"
+    },
+    {
+      value: "Revisiehistorie",
+      href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/commits"
+    },
+    {
+      value: "Pull requests",
+      href: "https://github.com/VNG-Realisatie/Actualisering-RSGB/pulls"
+    },
+    ]
+  }],
 
   maxTocLevel: 2,
 
